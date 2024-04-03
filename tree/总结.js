6e91d6add32fdab1237e3459eaeec15d17cde1bf
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-04-03 17:26:47 +0800

    Update 总结.js

--- tree/总结.js
+++ tree/总结.js
@@ -16,6 +16,26 @@ var walk = function(root) {
   return 结果
 };
 
+[404] 左叶子之和
+var sumOfLeftLeaves = function(root) {
+  let leftSum = 0
+  function travese(node){
+    if(node===null){
+      return 
+    }
+    // left是单独的节点
+    let left = node.left
+    if(left && !left.left && !left.right){
+      leftSum += left.val
+    }
+    // left有嵌套，走递归
+    travese(node.left)
+    travese(node.right)
+  }
+  travese(root) 
+  return leftSum
+};
+
 [100] 相同的树
 var isSameTree = function(p, q) {
   if(p===null&&q===null){
@@ -184,25 +204,9 @@ var diameterOfBinaryTree = function(root) {
   return len
 };
 
-[404] 左叶子之和
-var sumOfLeftLeaves = function(root) {
-  let leftSum = 0
-  function travese(node){
-    if(node===null){
-      return 
-    }
-    // left是单独的节点
-    let left = node.left
-    if(left && !left.left && !left.right){
-      leftSum += left.val
-    }
-    // left有嵌套，走递归
-    travese(node.left)
-    travese(node.right)
-  }
-  travese(root) 
-  return leftSum
-};
+
+
+
 
 
 
