e73a8e8a7819daf3118024a0f5f78896c82902c6
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-05-29 20:41:01 +0800

    Update 746.使用最小花费爬楼梯.js

--- dynamic/746.使用最小花费爬楼梯.js
+++ dynamic/746.使用最小花费爬楼梯.js
@@ -10,15 +10,15 @@
  * @return {number}
  */
 // 规划这个最小值
-// dp 你到哪第一个台阶所需要的最小花费 是dp[i]
 var minCostClimbingStairs = function(cost) {
-  // [10,15,20]
-  // dp其实不需要一个这么长的数组
-  let dp = [cost[0],cost[1]]
-  for(let i=2;i<cost.length;i++){
-    dp[i] = Math.min(dp[i-1] ,dp[i-2])+cost[i] 
-  }
-  return Math.min(dp[cost.length-1],dp[cost.length-2])
+    const n = cost.length;
+    function dfs(i) {
+        if (i <= 1) { // 递归边界
+            return 0;
+        }
+        return Math.min(dfs(i - 1) + cost[i - 1], dfs(i - 2) + cost[i - 2]);
+    }
+    return dfs(n);
 };
-// @lc code=end
+
 
