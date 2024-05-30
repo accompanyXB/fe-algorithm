56d1aa5daed31d43f25fbe124dc4dcec8611a27d
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-05-30 20:02:24 +0800

    Update 62.不同路径.js

--- dynamic/62.不同路径.js
+++ dynamic/62.不同路径.js
@@ -10,7 +10,10 @@
  * @param {number} n
  * @return {number}
  */
-//  [ 1, 1, 1, 1 ], [ 1, 000], [ 1, 000 ], [ 000, 1 ]
+初始化
+[['1', '1', '1'],
+ ['1', '', ''],
+ ['1', '', '']]
 var uniquePaths = function(m, n) {
   // const dp = new Array(n).fill(m).map(()=>{
   //   return new Array(n).fill(1)
@@ -24,6 +27,7 @@ var uniquePaths = function(m, n) {
   }
   for(let i=1;i<m;i++){
     for(let j=1;j<n;j++){
+      // 到达 (i, j) 的路径可以从 (i-1, j) 向下移动一步到达，或者从 (i, j-1) 向右移动一步到达。因此，路径数 dp[i][j] 就是这两种路径数的总和
       dp[i][j] = dp[i-1][j]+dp[i][j-1]
     }
   }
