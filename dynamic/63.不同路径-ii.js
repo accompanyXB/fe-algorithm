c552bf99ae8e9f0efc9348b8976fd47da776ba92
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-06-03 16:11:26 +0800

    Update 63.不同路径-ii.js

--- dynamic/63.不同路径-ii.js
+++ dynamic/63.不同路径-ii.js
@@ -10,45 +10,48 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
+  const m = obstacleGrid.length
+  const n = obstacleGrid[0].length
 
 //   dp = [  
 //   ['', '', ''],
 //   ['', '', ''],
 //   ['', '', '']
 // ]
+  
   const dp = new Array(m).fill('').map(() => new Array(n).fill(''));
-  
   // 初始化第一列
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = obstacleGrid[i][0] === 1 ? 0 : (i === 0 ? 1 : dp[i-1][0]);
+  for(let i=0;i<m&&obstacleGrid[i][0]===0;i++){
+    dp[i][0] = 1
   }
-  
+  for(let i=0;i<n&&obstacleGrid[0][i]===0;i++){
+    dp[0][i] = 1
+  }
+//   dp = [  
+//   [1, 1, 1],
+//   [1, '', ''],
+//   [1, '', '']
+// ]
+
 //   dp = [  
 //   [1, 1, 1],
 //   [1, 0, ''],
 //   [1, '', '']
 // ]
-  // 初始化第一行
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = obstacleGrid[0][j] === 1 ? 0 : (j === 0 ? 1 : dp[0][j-1]);
-  }
 
   // dp[1][1] = 0
   // dp[1][2] = dp[0][2] + dp[1][1] = 1 + 0 = 1
   // dp[2][1] = dp[1][1] + dp[2][0] = 0 + 1 = 1
   // dp[2][2] = dp[1][2] + dp[2][1] = 1 + 1 = 2
-
-  // 填充 dp 数组
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = obstacleGrid[i][j] === 1 ? 0 : dp[i-1][j] + dp[i][j-1];
+  for(let i=1;i<m;i++){
+    for(let j=1;j<n;j++){
+      dp[i][j] = obstacleGrid[i][j]===1
+                ? 0
+                : dp[i-1][j]+dp[i][j-1]
     }
   }
-  
-  return dp[m-1][n-1];
-};
+  return dp[m-1][n-1]
 
+};
 // @lc code=end
 
