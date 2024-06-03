d61a5261299f0d28861082815483e58192af409e
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-06-03 16:39:25 +0800

    Update 198.打家劫舍.js

--- dynamic/198.打家劫舍.js
+++ dynamic/198.打家劫舍.js
@@ -15,6 +15,10 @@ var rob = function(nums) {
   // 循环
     // dp递推公式
     // dp[i] 下边i之前的房屋，最多可以偷取dp[i]的钱
+
+   //   计算 dp[i] 时，有两种选择：
+  // 不偷第 i 个房屋，最大金额为 dp[i-1]。
+  // 偷第 i 个房屋，最大金额为 dp[i-2] + nums[i]（因为不能偷相邻房屋，所以要加上 i-2 的最大金额）。
     let dp = [nums[0], Math.max(nums[0],nums[1])]
     for(let i=2;i<nums.length;i++){
       dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i])
