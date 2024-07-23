d1ac41f662b66c8f708fc0102450034df3f6f6d5
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-07-23 11:40:48 +0800

    Update 209.长度最小的子数组.js

--- array/209.长度最小的子数组.js
+++ array/209.长度最小的子数组.js
@@ -29,25 +29,40 @@ var minSubArrayLen = function(target, nums) {
   // slow fast一起，fast走的快
   // [2,3,1,2,4,3]
   //    |     | 
+  
+  // let len = nums.length
+  // let slow = fast = 0
+  // let sum = 0
+  // let result = len+1
+  // while(fast<len){
+  //   // sum+=nums[fast++]
+  //   sum+=nums[fast]
+  //   fast++
+  //   while(sum>=target){
+  //     let subLen = fast-slow
+  //     result = (result<subLen)?result:subLen
+  //     // sum -=nums[slow++]
+  //     sum -=nums[slow]
+  //     slow++
+  //   }
+  // }
+  // return result>len?0:result
 
-  // 滑动窗口思路：先移动右边界，让窗口中的值满足题目的解，然后移动左窗口，找到最优解
-  let len = nums.length
-  let slow = fast = 0
-  let sum = 0
-  let result = len+1
-  while(fast<len){
-    // sum+=nums[fast++]
-    sum+=nums[fast]
-    fast++
-    while(sum>=target){
-      let subLen = fast-slow
-      result = (result<subLen)?result:subLen
-      // sum -=nums[slow++]
-      sum -=nums[slow]
-      slow++
-    }
+
+// 滑动窗口思路：先移动右边界，让窗口中的值满足题目的解，然后移动左窗口，找到最优解
+let left = 0, sum = 0;
+let minLength = Number.MAX_VALUE;
+for (let right = 0; right < nums.length; right++) {
+  // 由于数组中的所有数字都是正整数，因此在子数组中添加新的数字能得到更大的子数组之和
+  sum += nums[right];
+  // sum>=target 已经是找到了可行解了
+  while (left <= right && sum >= target) {
+    //  移动左边界，在可行解里面寻找最优解
+    minLength = Math.min(minLength, right - left + 1);
+    sum -= nums[left++];
   }
-  return result>len?0:result
+}
+  return minLength == Number.MAX_VALUE ? 0 : minLength;
 };
 // @lc code=end
 
