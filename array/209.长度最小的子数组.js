1e5eea092b2010a4d0bb5348282eefc38c3443e9
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-07-23 11:37:46 +0800

    Update 209.长度最小的子数组.js

--- array/209.长度最小的子数组.js
+++ array/209.长度最小的子数组.js
@@ -29,16 +29,22 @@ var minSubArrayLen = function(target, nums) {
   // slow fast一起，fast走的快
   // [2,3,1,2,4,3]
   //    |     | 
+
+  // 滑动窗口思路：先移动右边界，让窗口中的值满足题目的解，然后移动左窗口，找到最优解
   let len = nums.length
   let slow = fast = 0
   let sum = 0
   let result = len+1
   while(fast<len){
-    sum+=nums[fast++]
+    // sum+=nums[fast++]
+    sum+=nums[fast]
+    fast++
     while(sum>=target){
       let subLen = fast-slow
       result = (result<subLen)?result:subLen
-      sum -=nums[slow++]
+      // sum -=nums[slow++]
+      sum -=nums[slow]
+      slow++
     }
   }
   return result>len?0:result
