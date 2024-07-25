1a20980356f6101d7324dea0c9898538aa34c539
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-07-25 10:35:46 +0800

    Update 344.反转字符串.js

--- array/344.反转字符串.js
+++ array/344.反转字符串.js
@@ -14,9 +14,10 @@ var reverseString = function(s) {
   let left = 0
   let right = s.length-1
   while(left<=right){
-    let tmp = s[left]
-    s[left] = s[right]
-    s[right] = tmp
+    // let tmp = s[left]
+    // s[left] = s[right]
+    // s[right] = tmp
+    [s[left], s[right]] = [s[right], s[left]];
     left++
     right--
   }
