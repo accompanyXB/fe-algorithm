316e4cad1bdbd6332806343df828a590533e8924
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-08-06 15:23:48 +0800

    Update 92.反转链表-ii.js

--- linklist/92.反转链表-ii.js
+++ linklist/92.反转链表-ii.js
@@ -19,32 +19,43 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, left, right) {
+    // 如果 left是3 并且 right是7
+    // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+    // dummy -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
 
-  // if(left>=right){
-  //   return head
-  // }
-  let dummy ={
-    next:head
-  }
-  let tmp = dummy
-  for(let i=0;i<left-1;i++){
-    tmp = tmp.next
-  }
+    // 初始化 dummy 节点和找到 left 的前一个节点
+    let dummy ={
+        next:head
+    }
+    let tmp = dummy
+    // 找到 left 的前一个节点 tmp 2
+    for(let i=0;i<left-1;i++){
+        tmp = tmp.next
+    }
 
-  let prev = tmp.next
-  let cur = prev.next
+    let prev = tmp
+    let cur = tmp.next
 
-  for(let j=0;j<right-left;j++){
-    let next = cur.next
-    cur.next = prev
-    prev = cur
-    cur = next // cur = cur.next
-  }
-  
-  tmp.next.next = cur
-  tmp.next = prev
+    for(let j = 0; j < right - left + 1; j++){
+        let next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next // cur = cur.next
+    }
+    // dummy -> 1 -> 2
+    // 7 -> 6 -> 5 -> 4 -> 3
 
-  return dummy.next
+    // cur  -> 7 -> 6 -> 5 -> 4 -> 3
+    // next -> 8
+    // prev -> 7 -> 6 -> 5 -> 4 -> 3
+
+    // dummy -> 1 -> 2 -> 7 -> 6 -> 5 -> 4 -> 3 -> 8 -> 9
+    // 3 -> 8
+    tmp.next.next = cur
+    // 2 -> 7
+    tmp.next = prev
+
+    return dummy.next
 };
 // @lc code=end
 
