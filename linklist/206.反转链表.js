9f785823e55946111c518b0212545b86c52d688f
accompanyXB <52494159+accompanyXB@users.noreply.github.com>
2024-08-06 11:19:27 +0800

    Update 206.反转链表.js

--- linklist/206.反转链表.js
+++ linklist/206.反转链表.js
@@ -34,12 +34,12 @@ var reverseList = function(head) {
   let cur = head
   while(cur){
     // prev 上一个遍历的cur 也就是上一个节点
-    [cur.next,prev,cur] = [prev,cur,cur.next]
+    // [cur.next,prev,cur] = [prev,cur,cur.next]
 
-    // let next = cur.next
-    // cur.next = prev
-    // prev = cur //缓存上一次遍历的current
-    // cur = next //游标向下走
+    let next = cur.next
+    cur.next = prev
+    prev = cur // 整体 游标向下走
+    cur = next
   }
   return prev
 };
